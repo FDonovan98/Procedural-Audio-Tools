e9084275e6cbdc7e1af6a6cb77297cc1c2b9a22f
James Berry <git@jamesberry.co.uk>
2019-11-19 10:52:46 +0000

    Added scene for contract 1 and basic variable based pure tone generator

---
+++ Assets/Scripts/Contract1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contract1 : MonoBehaviour
+{
+    [SerializeField]
+    private int frequency;
+    [SerializeField]
+    private int sampleDurationSecs;
+    [SerializeField]
+    private int sampleRate;
+
+    private AudioSource audioSource;
+    private AudioClip outAudioClip;
+
+    public void PlayTone()
+    {
+        audioSource = GetComponent<AudioSource>();
+        outAudioClip = CreateToneAudioClip();
+        audioSource.PlayOneShot(outAudioClip);
+    }
+
+    private AudioClip CreateToneAudioClip()
+    {
+        int sampleLength = sampleRate * sampleDurationSecs;
+        float maxValue = 1f / 4f;
+
+        var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
+
+        float[] samples = new float[sampleLength];
+        for (var i = 0; i < sampleLength; i++)
+        {
+            float s = Mathf.Sin(2.0f * Mathf.PI * frequency * ((float)i / (float)sampleRate));
+            float v = s * maxValue;
+            samples[i] = v;
+        }
+
+        audioClip.SetData(samples, 0);
+        return audioClip;
+    }
+}
