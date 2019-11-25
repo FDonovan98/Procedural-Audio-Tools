45faaf15023a03e658f5a755ca23a2233c66c9e0
HDonovan96 <harry.bd11@gmail.com>
2019-11-25 20:01:00 +0000

    Created a basic tone generator for contract 4

---
+++ Assets/Scripts/Contract #4/ToneGenerator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ToneGenerator : MonoBehaviour
+{
+    private int position;
+    public float sampleDuration = 1.5f;
+    public int samplerate = 44100;
+    public int frequency = 440;
+    public string sampleName;
+
+    public void OnButtonPress()
+    {
+        AudioClip myClip = AudioClip.Create(sampleName, (int)(samplerate * sampleDuration), 1, samplerate, true, OnAudioRead, OnAudioSetPosition);
+        AudioSource aud = GetComponent<AudioSource>();
+        aud.clip = myClip;
+        aud.Play();
+    }
+
+    void OnAudioRead(float[] data)
+    {
+        int count = 0;
+        while (count < data.Length)
+        {
+            data[count] = Mathf.Sin(2 * Mathf.PI * frequency * position / samplerate);
+            position++;
+            count++;
+        }
+    }
+
+    void OnAudioSetPosition(int newPosition)
+    {
+        position = newPosition;
+    }
+}
