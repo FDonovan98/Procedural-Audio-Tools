ae91bfcf3b66be2e2c883150aa825af53d34c138
HDonovan96 <harry.bd11@gmail.com>
2019-11-25 20:26:11 +0000

    Merged code form 'ToneGenerator' script into 'ToneEditor' script

--- Assets/Scripts/Contract #4/Editor/ToneEditor.cs
+++ Assets/Scripts/Contract #4/Editor/ToneEditor.cs
@@ -3,9 +3,55 @@ using UnityEditor;
 
 public class ToneEditor : EditorWindow
 {
+    private int pos;
+    public float sampleDuration = 1.5f;
+    public int samplerate = 44100;
+    public int frequency = 440;
+    public string sampleName;
+    
     [MenuItem("Window/Tone Editor")]
     public static void ShowWindow()
     {
         GetWindow<ToneEditor>();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Button("Press me")
+        {
+            PlayAudio();
+        }
+    }
+
+    private void PlayAudio()
+    {
+        AudioClip audioclip = GenerateTone();
+        GameObject audioHolder = Instantiate(new GameObject());
+        AudioSource audioSource = audioHolder.AddComponent<AudioSource>();
+        audioSource.clip = audioclip;
+        audioSource.Play();
+    }
+
+    private AudioClip GenerateTone()
+    {
+        AudioClip audioClip = AudioClip.Create(sampleName, (int)(samplerate * sampleDuration), 1, samplerate, true, OnAudioRead, OnAudioSetPosition);
+
+        return audioClip;
+    }
+
+    void OnAudioRead(float[] data)
+    {
+        int count = 0;
+        while (count < data.Length)
+        {
+            data[count] = Mathf.Sin(2 * Mathf.PI * frequency * pos / samplerate);
+            pos++;
+            count++;
+        }
+    }
+
+    void OnAudioSetPosition(int newPosition)
+    {
+        pos = newPosition;
     }
 }
