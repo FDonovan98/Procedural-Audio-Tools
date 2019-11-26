f7e9d0d738fc8a2c8fe3808a8dec3581bbbd2eb1
James Berry <git@jamesberry.co.uk>
2019-11-26 09:47:19 +0000

    Added comments and DOCString for readability

--- Assets/Scripts/Contract1.cs
+++ Assets/Scripts/Contract1.cs
@@ -4,60 +4,90 @@ using UnityEngine;
 
 public class Contract1 : MonoBehaviour
 {
+    // How long each tone will play (currently not working)
     [SerializeField]
     private float sampleDurationSecs;
+    // Samples per second
     [SerializeField]
     private int sampleRate;
-    private float totalTime;
 
+    // Shows the tones played in the inspector (values change by code, altering them in inspector does nothing)
     [SerializeField]
     private float[] tones;
 
+    // Holds the component that plays the audio
     private AudioSource audioSource;
+    // Final audio tone sequence to play
     private AudioClip outAudioClip;
 
-    public void PlayTone()
+    private void Awake()
     {
+        // Finds the AudioSource Component
         audioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// Gets a new tone and plays it
+    /// </summary>
+    public void PlayTone()
+    { 
         outAudioClip = CreateToneAudioClip(tones);
+        // Plays tone once
         audioSource.PlayOneShot(outAudioClip);
     }
 
+    /// <summary>
+    /// Generates random tones (Currently hardcoded as 2)
+    /// Run when button is pressed
+    /// </summary>
     public void CoinPickupTone()
     {
+        // Array of frequencies to parse into audio tones
         tones = new float[2];
+        //For the sake of testing
+        //these frequencies are hardcoded such that
+        //one is 3 times more than the other which is chosen
+        //randomly. This is so that they are different enough
+        //that I can tell that it's working properly
         tones[0] = Random.Range(1000, 1200);
         tones[1] = tones[0] * 3f;        
         PlayTone();
     }
 
+
+    /// <summary>
+    /// Using an array input of tone frequencies, will create a sine wave pure tone
+    /// </summary>
+    /// <param name="frequency">Array of tones (as float values) to create a clip from</param>
+    /// <returns>AudioClip containing a sequence of tones that can be played through the AudioSource</returns>
     private AudioClip CreateToneAudioClip(float[] frequency)
     {
-        totalTime = 0;
+        // Computes length of sample
         int sampleLength = Mathf.RoundToInt(sampleRate * sampleDurationSecs * frequency.Length);
-        float maxValue = 1f / 4f;
+        // Volume multiplier (KEEP LOW TO KEEP EARS)
+        float volume = .25f;
 
-        var audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
-
+        // List to hold the sample values
         List<float> samples = new List<float>();
 
+        // Nested for loops find each point on a sound wave for each frequency parsed in
         for (int i = 0; i < frequency.Length; i++)
         {
-            for (var j = 0; j < sampleRate; j++)
+            for (int j = 0; j < sampleRate; j++)
             {
-                float s = Mathf.Sin(2.0f * Mathf.PI * frequency[i] * ((float)j / (float)sampleRate));
-                float v = s * maxValue;
-                samples.Add(v);
+                // Finds the values of each point on the sine wave of each tone according to sample rate
+                float pointOnWave = Mathf.Sin(2.0f * Mathf.PI * frequency[i] * ((float)j / (float)sampleRate));
+                // Adjusts volume and adds it to the list of samples
+                samples.Add(pointOnWave * volume);
             }
         }
 
+        // Creates an array with the same data as the sample list
         float[] tonesToPlay = new float[samples.Count];
+        tonesToPlay = samples.ToArray();
 
-        for (int s = 0; s < tonesToPlay.Length; s++)
-        {
-            tonesToPlay[s] = samples[s];
-        }
-        
+        // Creates the AudioClip that will be returned
+        AudioClip audioClip = AudioClip.Create("tone", sampleLength, 1, sampleRate, false);
         audioClip.SetData(tonesToPlay, 0);
         return audioClip;
     }
