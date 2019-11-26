c2d106318979549ebd452ce8d88c8d0dc62c7ccb
HDonovan96 <harry.bd11@gmail.com>
2019-11-26 16:57:31 +0000

    In 'ToneEditor' script added comments

--- Assets/Scripts/Contract #4/Editor/ToneEditor.cs
+++ Assets/Scripts/Contract #4/Editor/ToneEditor.cs
@@ -25,7 +25,7 @@ public class ToneEditor : EditorWindow
     GameObject audioHolder = null;
     AudioSource audioSource;
 
-    
+    // Adds the 'Tone Editor' option into the 'Window' toolbar in the unity editor.
     [MenuItem("Window/Tone Editor")]
     public static void ShowWindow()
     {
@@ -34,29 +34,41 @@ public class ToneEditor : EditorWindow
 
     void OnGUI()
     {
+        // Sets what editable fields are displayed in the window.
         sampleName = EditorGUILayout.TextField("Sample Name", sampleName);
         sampleFrequency = EditorGUILayout.IntField("Sample Frequency", sampleFrequency);
         inflection = EditorGUILayout.Toggle("Inflection", inflection);
+        // Only displays the 'End Frequency' field if 'Inflection' is set to true.
+        // This is for clarity as 'End Frequency' does nothing when 'Inflection' is false.
         if (inflection)
         {
             endFrequency = EditorGUILayout.IntField("End Frequency", endFrequency);
         }
+        // Sample duration is the time the tone lasts in seconds.
         sampleDuration = EditorGUILayout.FloatField("Sample Duration", sampleDuration);
         samplerate = EditorGUILayout.IntField("Sample Rate", samplerate);
         audioClip = (AudioClip)EditorGUILayout.ObjectField("Audio Clip", audioClip, typeof(AudioClip), true);
 
+        // Creates a 'Play Tone' button.
+        // When pressed generates a tone from the current settings and then plays it out loud.
         if (GUILayout.Button("Play Tone"))
         {
             GenerateTone();
             PlayAudio();
         }
 
+        // Creates a 'Save Tone' button.
+        // When pressed generates a tone from the current settings and then saves it.
+        // Tone is saved in 'Assets/Resources/Sounds' and 'sampleName is used as the file name.
         if (GUILayout.Button("Save Tone"))
         {
             GenerateTone();
             SaveTone();
         }
 
+        // Creates a 'Assign Current Tone To Selected Buttons' button.
+        // When pressed generates a tone from the current settings, then adds an OnClick() event to play the tone to any selected button.
+        // If the button doesn't already contain an 'AudioSource' component then one is added.
         if (GUILayout.Button("Assign Current Tone To Selected Buttons"))
         {
             GenerateTone();
@@ -64,43 +76,46 @@ public class ToneEditor : EditorWindow
         }
     }
 
+    // When the editor window is closed the gameobject used to allow the editor to play sounds is also destroyed.
     void OnDestroy()
     {
         DestroyImmediate(audioHolder);
     }
 
+    // Saves the current tone then gets every gameobject currently selected that has a 'Button' component.
+    // If the object already has an 'AudioSource' component then this is fetched, otherwise one is added.
+    // The tone that was saved at the beginning of the function is then loaded into the AudioSource clip.
+    // A persistent event is then added to the button to play the AudioSource clip on click.
     private void AssignToneToButtons()
     {
+        // Saves a tone using the current settings.
         SaveTone();
+        // Gets every object currently selected.
         foreach (GameObject obj in Selection.gameObjects)
         {
             Button button = obj.GetComponent<Button>();
+            // Ignores any objects that don't have a 'Button' component.
             if (button != null)
             {
-                AudioSource objAudioSource;
+                // Gets the 'AudioSource' component from the current gameobject.
+                AudioSource objAudioSource = obj.GetComponent<AudioSource>();
+                // If no 'AudioSource' component exists one is added.
+                // 'PlayOnAwake' is set to false so the sound is only heard when the button is pressed.
                 if (obj.GetComponent<AudioSource>() == null)
                 {
                     objAudioSource = obj.AddComponent<AudioSource>();
                     objAudioSource.playOnAwake = false;
                 }
-                else
-                {
-                    objAudioSource = obj.GetComponent<AudioSource>();
-                }
+                // Loads the saved audio clip into the 'AudioSource' clip parameter.
                 objAudioSource.clip = Resources.Load<AudioClip>("Sounds/" + sampleName);
-
+                // Adds a presistent listener onto the button to play the clip stored in the 'AudioSource' component.
                 UnityEventTools.AddPersistentListener(button.onClick, objAudioSource.Play);
 
             }
         }
     }
 
-    private void OnButtonPress()
-    {
-        Debug.Log("Cake");
-    }
-
-    // Creates the file structure then saves the current audio sample.
+    // Creates the file path then saves the current audio sample.
     private void SaveTone()
     {  
         CreateFileStructure();
@@ -110,37 +125,45 @@ public class ToneEditor : EditorWindow
     // Creates a folder to save any audio clips in.
     private void CreateFileStructure()
     {
-        // If the folder 'Assets/Sound' doesn't already exist it is created.
+        // If the folder 'Assets/Resources' doesn't already exist it is created.
         if (!Directory.Exists("Assets/Resources"))
         {
             AssetDatabase.CreateFolder("Assets", "Resources");
         }
+        // If the folder 'Assets/Resources/Sounds' doesn't already exist it is created.
         if (!Directory.Exists("Assets/Resources/Sounds"))
         {
             AssetDatabase.CreateFolder("Assets/Resources", "Sounds");
         }
     }
 
+    // Creates an empty game object to allow the editor to play sounds.
     private void PlayAudio()
     {
+        // If the required gameobject doesn't exist already it is created.
         if (audioHolder == null)
         {
             audioHolder = new GameObject();
+            // An 'AudioSource' component is needed to play audio so this is added to the empty gameobject.
             audioSource = audioHolder.AddComponent<AudioSource>();
         }
-         
+        // The AudioSource clip parameter is set to the last created audioClip and then played.
         audioSource.clip = audioClip;
         audioSource.Play();        
     }
 
-    private AudioClip GenerateTone()
+    // Creates an audio clip sampleDuration seconds in length.
+    // The generated clips frequency starts at sampleFrequency and ends at endFrequency.
+    private void GenerateTone()
     {
+        // readCounter is used when 'inflection' is set to true.
+        // It is needed for Mathf.Lerp to track how many times 'OnAudioRead' function has been run.
         readCounter = 0;
         audioClip = AudioClip.Create(sampleName, (int)(samplerate * sampleDuration), 1, samplerate, false, OnAudioRead, OnAudioSetPosition);
-
-        return audioClip;
     }
 
+    // Function is called in 'AudioClip.Create' in 'GenerateTone' function.
+    // Generates sound wave of the audio clip.
     void OnAudioRead(float[] data)
     {
         int count = 0;
@@ -148,8 +171,12 @@ public class ToneEditor : EditorWindow
         readCounter++;
         while (count < data.Length)
         {
+            // If inflection is set to true then the frequency is linearly interpolated between sampleFrequency and endFrequency.
+            // Otherwise, the frequency is just sampleFrequency.
             if (inflection)
             {
+                // The calculation for t is rough and loses accuracy as sampleDuration increases.
+                // It is accurate up to around two seconds, and is still accurate enough for longer.
                 currentFreq = Mathf.Lerp(sampleFrequency, endFrequency, (float)readCounter / (1 + 10 * sampleDuration)); 
             }
             else
@@ -162,7 +189,8 @@ public class ToneEditor : EditorWindow
             count++;
         }
     }
-
+    
+    // Function is called in 'AudioClip.Create' in 'GenerateTone' function.
     void OnAudioSetPosition(int newPosition)
     {
         pos = newPosition;
